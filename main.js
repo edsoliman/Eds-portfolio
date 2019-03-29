60e7b634c22a53515bfec9d774cd580e10fa395a
Ed Soliman <e.georgesoliman@gmail.com>
2019-03-28 19:04:39 -0700

    changed function names

--- main.js
+++ main.js
@@ -1,28 +1,28 @@
-function showAbout() {
+function handleAboutButton() {
     $('main').on('click', '#about_button', function(event) {
         $('main').find('#main_container_home').hide();
         $('main').find('#main_container_about').show();
     });
 };
 
-function showProjects() {
+function handleProjectsButton() {
     $('main').on('click', '#projects_button', function(event) {
         $('main').find('#main_container_home').hide();
         $('main').find('#main_container_projects').show();
     });
 };
 
-function showContact() {
+function handleContactButton() {
     $('main').on('click', '#contact_button', function(event) {
         $('main').find('#main_container_home').hide();
         $('main').find('#main_container_contact').show();
     });
 };
 
-function showHideLogic() {
-    showAbout();
-    showProjects();
-    showContact();
+function handleEdsPortfolio() {
+    handleAboutButton();
+    handleProjectsButton();
+    handleContactButton();
 };
 
-$(showHideLogic);
+$(handleEdsPortfolio);
